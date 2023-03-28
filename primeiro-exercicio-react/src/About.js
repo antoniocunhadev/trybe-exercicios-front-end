e48cc058db7274cac0125744f817d64f381447c4
Antonio Cunha <antonio.frango@gmail.com>
2023-03-28 15:33:17 -0300

    Ajustando arquivo

--- primeiro-exercicio-react/src/About.js
+++ primeiro-exercicio-react/src/About.js
@@ -4,13 +4,13 @@ class About extends React.Component {
   render() {
     return (
       <div>
-        <h1>{"Antonio Cunha"}</h1>
-        <p>{"Sou Desenvolvedor Web, tenho 43 anos e vivo em Barueri-SP"}</p>
-        <h2>{"Minhas Habilidades"}</h2>
+        <h1>"Antonio Cunha"</h1>
+        <p>"Sou Desenvolvedor Web, tenho 43 anos e vivo em Barueri-SP"</p>
+        <h2>"Minhas Habilidades"</h2>
         <ul>
-          <li>{"HTML5"}</li>
-          <li>{"CSS"}</li>
-          <li>{"JavaScript"}</li>
+          <li>"HTML5"</li>
+          <li>"CSS"</li>
+          <li>"JavaScript"</li>
         </ul>
       </div>
     );
