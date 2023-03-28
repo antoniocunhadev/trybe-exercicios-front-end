1318e26f7df82b54869901f520411bc767104d97
Antonio Cunha <antonio.frango@gmail.com>
2023-03-28 15:19:06 -0300

    Primeiro exercicio React :rocket:

---
+++ primeiro-exercicio-react/src/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{"Antonio Cunha"}</h1>
+        <p>{"Sou Desenvolvedor Web, tenho 43 anos e vivo em Barueri-SP"}</p>
+        <h2>{"Minhas Habilidades"}</h2>
+        <ul>
+          <li>{"HTML5"}</li>
+          <li>{"CSS"}</li>
+          <li>{"JavaScript"}</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default About;
