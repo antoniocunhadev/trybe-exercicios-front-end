8ff6f92ffc95a42b425273a2f05ac6f55e651c41
Antonio Cunha <antonio.frango@gmail.com>
2023-04-07 17:49:14 -0300

    alterando a cor do botoes

--- componentes-estados/src/App.js
+++ componentes-estados/src/App.js
@@ -11,14 +11,14 @@ class App extends React.Component {
       numeroDeCliques1: 0,
       numeroDeCliques2: 0,
       numeroDeCliques3: 0,
-    }
-    
+    } 
   }
+
   handleButtonOne() {
     this.setState ((estadoAnterior) => ({
       numeroDeCliques1: estadoAnterior.numeroDeCliques1 + 1
     }))
-    console.log('Clicou no botão 1!', this);
+    console.log('Clicou no botão 1!', this)
   }
 
   handleButtonTwo() {
@@ -35,15 +35,27 @@ class App extends React.Component {
     console.log('Clicou no botão 3!', this);
   }
 
+  getButtonColor(num) {
+    return num % 2 === 0 ? 'green' : 'white';
+  }
+
   render() {
     const { numeroDeCliques1 } = this.state
     const { numeroDeCliques2 } = this.state
     const { numeroDeCliques3 } = this.state
     return (
       <div>
-        <button type="button" onClick={ this.handleButtonOne }>{ numeroDeCliques1 }</button>
-        <button type="button" onClick={ this.handleButtonTwo }>{`Cliques botão 2: ${numeroDeCliques2}` }</button>
-        <button type="button" onClick={ this.handleButtonThree }>{ numeroDeCliques3 }</button>
+        <button type="button" onClick={ this.handleButtonOne } 
+        style={ { backgroundColor: this.getButtonColor(numeroDeCliques1) } }
+        >
+        {`Cliques botão 1:  ${numeroDeCliques1} `}</button>
+        
+        <button type="button" onClick={ this.handleButtonTwo } 
+        style={ { backgroundColor: this.getButtonColor(numeroDeCliques2) } }
+        >{`Cliques botão 2: ${numeroDeCliques2}` }</button>
+        
+        <button type="button" onClick={ this.handleButtonThree } style={ { backgroundColor: this.getButtonColor(numeroDeCliques3) } }
+        >{`Cliques botão 3: ${numeroDeCliques3}` }</button>
       </div>
     );
   }
